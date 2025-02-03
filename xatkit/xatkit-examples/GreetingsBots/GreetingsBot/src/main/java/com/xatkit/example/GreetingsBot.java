c7dcc820cbbb5774952e162709148a415142b77f
Dilini Shanika <diliniwickramathunga@gmail.com>
2025-02-03 22:41:22 +0530

    Add more Intents to greetings chatbot

--- xatkit/xatkit-examples/GreetingsBots/GreetingsBot/src/main/java/com/xatkit/example/GreetingsBot.java
+++ xatkit/xatkit-examples/GreetingsBots/GreetingsBot/src/main/java/com/xatkit/example/GreetingsBot.java
@@ -15,29 +15,11 @@ import static com.xatkit.dsl.DSL.intentIs;
 import static com.xatkit.dsl.DSL.model;
 import static com.xatkit.dsl.DSL.state;
 
-/**
- * This is an example greetings bot designed with Xatkit.
- * <p>
- * You can check our <a href="https://github.com/xatkit-bot-platform/xatkit/wiki">wiki</a>
- * to learn more about bot creation, supported platforms, and advanced usage.
- */
 public class GreetingsBot {
 
-    /*
-     * Your bot is a plain Java application: you need to define a main method to make the created jar executable.
-     */
     public static void main(String[] args) {
 
-        /*
-         * Define the intents our bot will react to.
-         * <p>
-         * In this example we want our bot to answer greetings inputs and "how are you" questions, so we create an
-         * intent for each, and we give a few example training sentences to configure the underlying NLP engine.
-         * <p>
-         * Note that we recommend the usage of Lombok's val when using the Xatkit DSL: the fluent API defines many
-         * interfaces that are not useful for bot designers. If you don't want to use val you can use our own
-         * interface IntentVar instead.
-         */
+        // Define multiple intents for various greetings and general questions
         val greetings = intent("Greetings")
                 .trainingSentence("Hi")
                 .trainingSentence("Hello")
@@ -49,95 +31,74 @@ public class GreetingsBot {
                 .trainingSentence("What's up?")
                 .trainingSentence("How do you feel?");
 
-        /*
-         * Instantiate the platform we will use in the bot definition.
-         * <p>
-         * Instantiating the platform before specifying the bot's states creates a usable reference that can be
-         * accessed in the states, e.g:
-         * <pre>
-         * {@code
-         * myState
-         *   .body(context -> reactPlatform.reply(context, "Hi, nice to meet you!");
-         * }
-         * </pre>
-         */
-        /*
-         * Similarly, instantiate the intent/event providers we want to use.
-         * <p>
-         * In our example we want to receive intents (i.e. interpreted user inputs) from our react client, so we
-         * create a ReactIntentProvider instance. We also want to receive events from the react client (e.g. when the
-         * client's connection is ready), so we create a ReactEventProvider instance.
-         * <p>
-         * We can instantiate as many providers as we want, including providers from different platforms.
-         */
+        // Adding additional intents for other general questions
+        val nameIntent = intent("Name")
+                .trainingSentence("What is your name?")
+                .trainingSentence("Who are you?")
+                .trainingSentence("What should I call you?");
+        
+        val ageIntent = intent("Age")
+                .trainingSentence("How old are you?")
+                .trainingSentence("What is your age?")
+                .trainingSentence("How many years have you lived?");
+        
+        val ownerIntent = intent("Owner")
+                .trainingSentence("Who is your owner?")
+                .trainingSentence("Do you have an owner?")
+                .trainingSentence("Who created you?");
+        
+        val hobbyIntent = intent("Hobby")
+                .trainingSentence("What are your hobbies?")
+                .trainingSentence("What do you like to do?")
+                .trainingSentence("Do you have any hobbies?");
+
+        // More intents (Repeat this structure for other intents)
+        val weatherIntent = intent("Weather")
+                .trainingSentence("What is the weather like today?")
+                .trainingSentence("Is it sunny outside?")
+                .trainingSentence("What is the temperature?");
+        
+        val timeIntent = intent("Time")
+                .trainingSentence("What time is it?")
+                .trainingSentence("Can you tell me the time?")
+                .trainingSentence("What’s the current time?");
+        
+        // Instantiate the platform we will use in the bot definition.
         ReactPlatform reactPlatform = new ReactPlatform();
         ReactEventProvider reactEventProvider = reactPlatform.getReactEventProvider();
         ReactIntentProvider reactIntentProvider = reactPlatform.getReactIntentProvider();
 
-        /*
-         * Create the states we want to use in our bot.
-         * <p>
-         * Similarly to platform/provider creation, we create the state variables first, and we specify their content
-         * later. This allows to define circular references between states (e.g. AwaitingQuestion -> HandleWelcome ->
-         * AwaitingQuestion).
-         * <p>
-         * This is not mandatory though, the important point is to have fully specified states when we build the
-         * final bot model.
-         */
+        // Define states
         val init = state("Init");
         val awaitingInput = state("AwaitingInput");
         val handleWelcome = state("HandleWelcome");
         val handleWhatsUp = state("HandleWhatsUp");
+        val handleName = state("HandleName");
+        val handleAge = state("HandleAge");
+        val handleOwner = state("HandleOwner");
+        val handleHobby = state("HandleHobby");
+        val handleWeather = state("HandleWeather");
+        val handleTime = state("HandleTime");
 
-        /*
-         * Specify the content of the bot states (i.e. the behavior of the bot).
-         * <p>
-         * Each state contains:
-         * <ul>
-         * <li>An optional body executed when entering the state. This body is provided as a lambda expression
-         * with a context parameter representing the current state of the bot.</li>
-         * <li>A mandatory list of next() transitions that are evaluated when a new event is received. This list
-         * must contain at least one transition. Transitions can be guarded with a when(...) clause, or
-         * automatically navigated using a direct moveTo(state) clause.</li>
-         * <li>An optional fallback executed when there is no navigable transition matching the received event. As
-         * for the body the state fallback is provided as a lambda expression with a context parameter representing
-         * the current state of the bot. If there is no fallback defined for a state the bot's default fallback state
-         * is executed instead.
-         * </li>
-         * </ul>
-         */
+        // Define transitions between states
         init
                 .next()
-                /*
-                 * We check that the received event matches the ClientReady event defined in the
-                 * ReactEventProvider. The list of events defined in a provider is available in the provider's
-                 * wiki page.
-                 */
                 .when(eventIs(ReactEventProvider.ClientReady)).moveTo(awaitingInput);
 
         awaitingInput
                 .next()
-                /*
-                 * The Xatkit DSL offers dedicated predicates (intentIs(IntentDefinition) and eventIs
-                 * (EventDefinition) to check received intents/events.
-                 * <p>
-                 * You can also check a condition over the underlying bot state using the following syntax:
-                 * <pre>
-                 * {@code
-                 * .when(context -> [condition manipulating the context]).moveTo(state);
-                 * }
-                 * </pre>
-                 */
                 .when(intentIs(greetings)).moveTo(handleWelcome)
-                .when(intentIs(howAreYou)).moveTo(handleWhatsUp);
+                .when(intentIs(howAreYou)).moveTo(handleWhatsUp)
+                .when(intentIs(nameIntent)).moveTo(handleName)
+                .when(intentIs(ageIntent)).moveTo(handleAge)
+                .when(intentIs(ownerIntent)).moveTo(handleOwner)
+                .when(intentIs(hobbyIntent)).moveTo(handleHobby)
+                .when(intentIs(weatherIntent)).moveTo(handleWeather)
+                .when(intentIs(timeIntent)).moveTo(handleTime);
 
         handleWelcome
                 .body(context -> reactPlatform.reply(context, "Hi, nice to meet you!"))
                 .next()
-                /*
-                 * A transition that is automatically navigated: in this case once we have answered the user we
-                 * want to go back in a state where we wait for the next intent.
-                 */
                 .moveTo(awaitingInput);
 
         handleWhatsUp
@@ -145,30 +106,41 @@ public class GreetingsBot {
                 .next()
                 .moveTo(awaitingInput);
 
-        /*
-         * The state that is executed if the engine doesn't find any navigable transition in a state and the state
-         * doesn't contain a fallback.
-         * <p>
-         * The default fallback state is typically used to answer generic error messages, while state fallback can
-         * benefit from contextual information to answer more precisely.
-         * <p>
-         * Note that every Xatkit bot needs a default fallback state.
-         */
-        val defaultFallback = fallbackState()
-                .body(context -> reactPlatform.reply(context, "Sorry, I didn't, get it"));
+        handleName
+                .body(context -> reactPlatform.reply(context, "My name is XatkitBot. Nice to meet you!"))
+                .next()
+                .moveTo(awaitingInput);
 
-        /*
-         * Creates the bot model that will be executed by the Xatkit engine.
-         * <p>
-         * A bot model contains:
-         * - A list of platforms used by the bot. Xatkit will take care of starting and initializing the platforms
-         * when starting the bot.
-         * - A list of providers the bot should listen to for events/intents. As for the platforms Xatkit will take
-         * care of initializing the provider when starting the bot.
-         * - The entry point of the bot (a.k.a init state). The other states will be automatically collected by analyzing the state machine
-         * - The default fallback state: the state that is executed if the engine doesn't find any navigable
-         * transition in a state and the state doesn't contain a fallback.
-         */
+        handleAge
+                .body(context -> reactPlatform.reply(context, "I am timeless, but I was created recently!"))
+                .next()
+                .moveTo(awaitingInput);
+
+        handleOwner
+                .body(context -> reactPlatform.reply(context, "I was created by a developer using Xatkit!"))
+                .next()
+                .moveTo(awaitingInput);
+
+        handleHobby
+                .body(context -> reactPlatform.reply(context, "I enjoy chatting and helping people!"))
+                .next()
+                .moveTo(awaitingInput);
+
+        handleWeather
+                .body(context -> reactPlatform.reply(context, "Sorry, I can't check the weather, but you can look it up!"))
+                .next()
+                .moveTo(awaitingInput);
+
+        handleTime
+                .body(context -> reactPlatform.reply(context, "I don't have a watch, but you can check your device!"))
+                .next()
+                .moveTo(awaitingInput);
+
+        // Default fallback
+        val defaultFallback = fallbackState()
+                .body(context -> reactPlatform.reply(context, "Sorry, I didn't get that"));
+
+        // Define the bot model
         val botModel = model()
                 .usePlatform(reactPlatform)
                 .listenTo(reactEventProvider)
@@ -177,17 +149,7 @@ public class GreetingsBot {
                 .defaultFallbackState(defaultFallback);
 
         Configuration botConfiguration = new BaseConfiguration();
-        /*
-         * Add configuration properties (e.g. authentication tokens, platform tuning, intent provider to use).
-         * Check the corresponding platform's wiki page for further information on optional/mandatory parameters and
-         * their values.
-         */
-
         XatkitBot xatkitBot = new XatkitBot(botModel, botConfiguration);
         xatkitBot.run();
-        /*
-         * The bot is now started, you can check http://localhost:5000/admin to test it.
-         * The logs of the bot are stored in the logs folder at the root of this project.
-         */
     }
 }
